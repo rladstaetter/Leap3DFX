f8c7929dbbfebf66248d2c72dc7f1b72c01e418d
Robert Ladstätter <rladstaetter@gmail.com>
2014-02-15 18:29:21 +0100

    mavenized project

---
+++ lib/install_maven.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=LeapJava.jar -DgroupId=com.leapmotion -DartifactId=leap -Dversion=1.0.9.8391 -Dpackaging=jar
+mvn install:install-file -Dfile=jim3dsModelImporterJFX.jar -DgroupId=com.interactivemesh -DartifactId=3dsmodelimporter -Dversion=EA_2014-02-09 -Dpackaging=jar
